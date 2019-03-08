1f235df2123964f6299d8e116466bd6a83c438b1
Charles Pache <xhenos76@gmail.com>
2019-03-07 20:59:53 -0600

    added JSON components and notes

--- MapImageTileTool/MapImageTileTool/Program.cs
+++ MapImageTileTool/MapImageTileTool/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.IO;
 using System.Drawing;
+using System.Text;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MapImageTileTool
 {
+    class MapInfo
+    {
+        public string MapName { get; set; }
+        public decimal Scale { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int OffsetX { get; set; }
+        public int OffsetY { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            JObject mapSetInfo;
+            //MapInfo currMapInfo;
 
             //select map depo
             Console.Write("Name of Map Folder: ");
@@ -19,6 +35,8 @@ namespace MapImageTileTool
             {
                 Directory.SetCurrentDirectory(depoName);
             }
+
+            // Map folder doesn't exist, create new one
             else
             {
                 Console.WriteLine("No folder " + depoName + " found.");
@@ -28,11 +46,18 @@ namespace MapImageTileTool
                 {
                     Directory.CreateDirectory(depoName);
                     Directory.SetCurrentDirectory(depoName);
+                    //MapInfo emptyMapInfo = new MapInfo();
+                    File.WriteAllText(@"MapInfo.JSON", "{\nMaps:[]\n}");
                 }
                 else
                 {
-                    System.Environment.Exit(1);
+                    Environment.Exit(1);
                 }
+            }
+
+            using (StreamReader reader = File.OpenText(@"MapInfo.JSON"))
+            {
+                mapSetInfo = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
             }
 
             int menuSelection = 0;
@@ -54,7 +79,13 @@ namespace MapImageTileTool
                     switch (menuSelection)
                     {
                         case 1:
-                            addMap();
+                            Console.WriteLine("Enter image name: ");
+                            string fileName = Console.ReadLine();
+
+                            // make sure Map Folder can be used, and write the selected image to tiles
+                            if (GetMapFolder(fileName))
+                                WriteMapTiles(fileName, mapSetInfo);
+
                             break;
                         case 2:
 
@@ -67,18 +98,6 @@ namespace MapImageTileTool
                 }
             }
 
-            
-
-            Console.WriteLine("\n - Map Offsets - \n");
-
-            Console.Write("Selected Map X: ");
-            int mapX = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Selected Map Y: ");
-            int mapY = Convert.ToInt32(Console.ReadLine());
-
-            
-
             // select map depo
             // add new map
             // map gets tiled and named, put into folder
@@ -86,24 +105,12 @@ namespace MapImageTileTool
             // select map folder
             // generate JSON string with file names, width, height, depth and current displayed board
             // write new JSON file
-            
-
-            
-            Console.ReadLine();
         }
-        static void addMap()
+
+        // returns 'true' if image and its associated map folder exists, 'false' if it doesn't exist, and 'false' if the file is not allowed to be overwritten
+        static bool GetMapFolder(string fileName)
         {
             Console.Write("Select image file in folder: ");
-            string fileName = Console.ReadLine();
-
-            Console.Write("Width: ");
-            int width = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Depth: ");
-            int depth = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Scale level (starting at tactical level, 0): ");
-            int zLevel = Convert.ToInt32(Console.ReadLine());
 
             // check file name
             if (File.Exists(fileName + ".png"))
@@ -113,8 +120,62 @@ namespace MapImageTileTool
                 {
                     Directory.CreateDirectory(fileName + " Maps");
                     Console.WriteLine(fileName + " Maps created");
+                    return true;
+                }
+
+                // if directory exists, ask to overwrite
+                else
+                {
+                    Console.WriteLine("/" + fileName + " Maps exists. Overwrite maps? (Y or N): ");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
                 }
             }
+            else
+            {
+                Console.WriteLine(fileName + ".png" + " not found.");
+                return false;
+            }
+        }
+
+        // writeMapTiles creates the image tiles and writes the MapInfo to the JSON file
+        static void WriteMapTiles(string fileName, JObject mapSetInfo)
+        {
+            Console.Write("Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Scale amount: ");
+            decimal scale = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n - Map Offsets - \n");
+
+            Console.Write("Selected Map X: ");
+            int mapX = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Selected Map Y: ");
+            int mapY = Convert.ToInt32(Console.ReadLine());
+
+
+            MapInfo mapInfo = new MapInfo()
+            {
+                Width = width,
+                Height = height,
+                Scale = scale,
+                OffsetX = mapX,
+                OffsetY = mapY
+            };
+
+            JArray JSONmapArray = (JArray)mapSetInfo["Maps"];
+            JSONmapArray.Add(JsonConvert.SerializeObject(mapInfo));
+
+            /*
 
             using (FileStream pngStream = new FileStream(fileName + ".png", FileMode.Open, FileAccess.Read))
             using (var image = new Bitmap(pngStream))
@@ -123,25 +184,16 @@ namespace MapImageTileTool
                 System.Drawing.Imaging.PixelFormat format = image.PixelFormat;
                 for (int i = 0; i < width; i++)
                 {
-                    for (int j = 0; j < depth; j++)
+                    for (int j = 0; j < height; j++)
                     {
-                        Rectangle cropArea = new Rectangle(i * (image.Width / width), j * (image.Height / depth), (image.Width / width), (image.Height / depth));
+                        Rectangle cropArea = new Rectangle(i * (image.Width / width), j * (image.Height / height), (image.Width / width), (image.Height / height));
                         Bitmap bitmap = image.Clone(cropArea, format);
                         bitmap.Save(fileName + "_" + i + "_" + j + ".png");
                     }
                 }
             }
+
+            */
         }
     }
-
-    class MapInfo
-    {
-        public string MapName;
-        public int Scale;
-        public int Width;
-        public int Height;
-        public int OffsetX;
-        public int OffsetY;
-    }
-
 }
