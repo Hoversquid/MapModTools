dcb7c2ffee9d49a30e7f68db18339b9771f20a6e
Alex Pace <xhenos76@gmail.com>
2019-03-28 10:33:46 -0500

    java version commit

---
+++ MapImageTileTool/MapImageTileTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MapImageTileTool
+{
+    class Program
+    {
+        static MapReader reader;
+
+        static void Main(string[] args)
+        {
+            reader = new MapReader();
+            Console.WriteLine("Program has concluded, press any key to exit.");
+            Console.ReadKey();
+            Environment.Exit(1);
+        }
+    }
+}
